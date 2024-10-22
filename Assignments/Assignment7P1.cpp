4a4ede2f7ebc7ec461b05f916953f2d345cd6cbb
matthewbrinkman <mbrinkma@my.athens.edu>
2024-10-22 17:57:24 -0500

    Update Assignment7P1.cpp

--- Assignments/Assignment7P1.cpp
+++ Assignments/Assignment7P1.cpp
@@ -6,6 +6,27 @@ using namespace std;
 
 template <typename T>
 bool binarySearch(vector<T> aSortedVector, T itemToBeFound, bool printDebugData = false) {
+private:
+    bool searchHelper(TreeNode* node, int value) {
+        if (node == nullptr) {
+            return false;
+        }
+        
+        if (node->data == value) {
+            return true;
+        }
+        
+        if (value < node->data) {
+            return searchHelper(node->left, value);
+        }
+        return searchHelper(node->right, value);
+    }
+    
+public:
+    bool search(int value) {
+        return searchHelper(root, value);
+    }
+    
     int left = 0;
     int right = aSortedVector.size() - 1;
     int itemsExamined = 0;
